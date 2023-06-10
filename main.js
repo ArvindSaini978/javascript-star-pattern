9b5be18a22f7c77fa79723eb6df7ae9e66f632e4
Arvind <arvindsaini1202@outlook.com>
2023-06-10 08:36:54 +0530

    Added Loading page

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const closeRunnerContainerBtn = document.querySelectorAll(".close-runner-contain
 const runnerContainer = document.querySelectorAll(".runner-container");
 const iframes = document.querySelectorAll("iframe");
 const runNow = document.querySelectorAll(".run-now");
+const loader = document.getElementById("loader");
 
 runNow.forEach((element, index) => {
     element.onclick = ()=>{
@@ -15,4 +16,10 @@ closeRunnerContainerBtn.forEach((closeBtn, index) => {
         iframes[index].src = iframes[index].src;
     }
 });
+window.onload = ()=>{
+    setTimeout(() => {
+        loader.style.display = "none";
+        document.body.classList.add("active");
+    }, 500);
+}
 
