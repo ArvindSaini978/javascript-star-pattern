3d0ec8823407348df573b47a7e2ef5e46695f38c
Arvind <arvindsaini1202@outlook.com>
2023-06-24 18:17:20 +0530

    added mobile navigation menu (table of content)

--- main.js
+++ main.js
@@ -3,6 +3,33 @@ const runnerContainer = document.querySelectorAll(".runner-container");
 const iframes = document.querySelectorAll("iframe");
 const runNow = document.querySelectorAll(".run-now");
 const loader = document.getElementById("loader");
+const toggleSidebar = document.getElementById("burger");
+const sidebar = document.getElementById("sidebar");
+const closeSidebar = document.getElementById("closeSidebar");
+const sidebarLinksCont = document.getElementById("sidebarLinksCont");
+
+toggleSidebar.addEventListener("click", ()=>{
+    setTimeout(() => {
+        sidebar.classList.toggle("activeSidebar");
+    }, 150);
+})
+closeSidebar.addEventListener("click", ()=>{
+    setTimeout(() => {
+        sidebar.classList.remove("activeSidebar");
+        setTimeout(() => {
+            sidebarLinksCont.scrollTop = 0;
+        }, 300);
+    }, 150);
+})
+document.addEventListener("click", (e)=>{
+    let targetId = e.target.id;
+    if ((targetId !=="sidebar") && (targetId !=="burger") && (targetId !=="line") && (targetId !=="closeSidebar") && (targetId !=="sidebar") && (targetId !=="sidebarH2") && (targetId !=="sidebarTop") && (targetId !=="seperatorSide") && (targetId !=="SidebarLinksCont")) {
+        sidebar.classList.remove("activeSidebar");
+        setTimeout(() => {
+            sidebarLinksCont.scrollTop = 0;
+        }, 300);
+    }
+})
 
 runNow.forEach((element, index) => {
     element.onclick = ()=>{
@@ -24,10 +51,19 @@ window.onload = ()=>{
 }
 document.onscroll = (e)=>{
     if (window.scrollY>64) {
-        document.getElementById("navbar").classList.add("active")
+        document.getElementById("navbar").classList.add("active");
     }
     if (window.scrollY<64) {
-        document.getElementById("navbar").classList.remove("active")
+        document.getElementById("navbar").classList.remove("active");
+    }
+}
+
+window.onresize = ()=>{
+    if (window.innerWidth>1143) {
+        sidebar.classList.remove("activeSidebar");
+        setTimeout(() => {
+            sidebarLinksCont.scrollTop = 0;
+        }, 300);
     }
 }
 
